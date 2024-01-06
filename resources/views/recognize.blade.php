b39a58e8e636b7935b2943bd5a200a45f54dcdf8
Md. Masudul Kabir <masud.ncse@gmail.com>
2024-01-07 02:33:54 +0600

    Migration & Seeder

--- resources/views/recognize.blade.php
+++ resources/views/recognize.blade.php
@@ -1,12 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                Recognize
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
+        <div class="grid grid-cols-12 gap-4">
+            <div class="col-span-8 flex justify-center">
+                {{--<img src="{{ asset('img/door-open.png') }}" alt="open">--}}
+                <img src="{{ asset('img/door-close.png') }}" alt="close">
+            </div>
+            <div class="col-span-4">
+                <video id="webcam" width="640" height="480" autoplay></video>
+                <button id="capture">Capture Screenshot</button>
+                <canvas id="screenshot" width="640" height="480"></canvas>
             </div>
         </div>
     </div>
+@endsection
 
+@section('scripts')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // Get access to the user's camera
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    // Display the webcam feed in a video element
+                    $('#webcam').get(0).srcObject = stream;
+                })
+                .catch(function (error) {
+                    console.error('Error accessing webcam:', error);
+                });
+
+            // Handle the capture button click
+            $('#capture').on('click', function() {
+                // Capture a screenshot from the video and display it on a canvas
+                const video = $('#webcam').get(0);
+                const canvas = $('#screenshot').get(0);
+                const context = canvas.getContext('2d');
+
+                // Draw the current frame from the video onto the canvas
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                // Do something with the screenshot data (e.g., send it to the server)
+                const screenshotData = canvas.toDataURL('image/png');
+                console.log('Screenshot captured:', screenshotData);
+            });
+        });
+    </script>
 @endsection
